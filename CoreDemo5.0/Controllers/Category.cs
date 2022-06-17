5c59839d8856926d357a1fd9ceaa1884f4e7c9fa
Enver2779 <18701027@mersin.edu.tr>
2022-06-17 12:56:09 +0300

    Murat Yücedağ ile asp.net core5.0 ilk ödev
    .net core 5.0 ile c# diliyle N- katmanlı mimari giriş kısmı ilk 20

---
+++ CoreDemo5.0/Controllers/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo5._0.Controllers
+{
+    public class Category : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
